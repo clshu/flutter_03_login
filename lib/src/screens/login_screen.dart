6e3240fe8952a5d1eb9098bf2d23138b950f9535
clshu <clshu2020@gmail.com>
2021-06-09 00:55:37 -0700

    1. Create LoginScreen class. 2. Add a Form. 3. Add an email field and set keyboard type to email type.

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,38 @@
+import "package:flutter/material.dart";
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: [
+            emailField(),
+            // passwordField(),
+            // submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+       labelText: 'Email Address',
+       hintText: 'you@example.com', 
+      ),
+    );
+  }
+
+  // Widget passwordField() {}
+
+  // Widget submitButton() {}
+}
